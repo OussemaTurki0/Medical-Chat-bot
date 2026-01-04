f71773d5b2880efd88dc50e4873dddef223c8b58
oussematurki0 <oussematurki0@gmail.com>
2026-01-04 16:06:48 +0100

    Remove large files and clean repo

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Ai Chat-Bot',
+    version= '0.0.0',
+    author= 'Oussema turki',
+    author_email= 'oussematurki0@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
